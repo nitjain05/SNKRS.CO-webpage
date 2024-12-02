6cfcb9b7c00e2d4a837e18066edeefd5e8e3ce2f
nitjain05 <jainnit4@gmail.com>
2024-12-02 15:18:00 +0530

    Delete Nike-Landing-Page-main directory

--- Nike-Landing-Page-main/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.product-images img').forEach(img => {
-    img.addEventListener('click', function() {
-        document.querySelector('.product-main-image img').src = this.src;
-    });
-});
